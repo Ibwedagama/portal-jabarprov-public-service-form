9b39d6be3de5e3ac7e780e84400610968518f496
Ibwedagama <wardana.wedagama@gmail.com>
2022-10-31 14:05:46 +0700

    refactor: add accept type to input file

--- components/Base/ImageUpload/index.vue
+++ components/Base/ImageUpload/index.vue
@@ -1,7 +1,7 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div
-    class="w-full h-[500px] bg-gray-200 flex items-center justify-center rounded-xl"
+    class="w-full h-[400px] bg-gray-200 flex items-center justify-center rounded-xl"
     @drop.prevent="onDrop"
   >
     <div class="text-center">
@@ -15,6 +15,7 @@
         id="upload-button"
         type="file"
         hidden
+        accept="image/*"
         @input="onInputFile"
       >
       <label
@@ -27,8 +28,19 @@
         id="file-chosen"
         class="ml-2"
       >
-        No file chosen
+        {{ props.filename }}
       </p>
+    </div>
+  </div>
+
+  <div
+    v-show="state.isError === 'NOT_IMAGE'"
+    class="toast toast-top toast-end"
+  >
+    <div class="alert alert-error">
+      <div>
+        <span>Error! File yang anda masukan bukan Gambar!</span>
+      </div>
     </div>
   </div>
 </template>
@@ -37,31 +49,46 @@
 import { onMounted, onUnmounted } from 'vue'
 const emit = defineEmits(['files-dropped'])
 
+const props = defineProps({ filename: String })
+
+const state = reactive({ isError: '' })
+
 function onDrop(e) {
-    emit('files-dropped', [...e.dataTransfer.files])
-    console.log([...e.dataTransfer.files])
+  if (!isImage(e.dataTransfer.files[0])) {
+    state.isError = 'NOT_IMAGE'
+
+    setTimeout(() => {
+      state.isError = ''
+    }, 2000);
+    return 
+  }
+
+  emit('files-dropped', e.dataTransfer.files[0])
 }
 
 function onInputFile(e) {
-    emit('files-dropped', [...e.target.files])
-    console.log([...e.target.files])
+  emit('files-dropped', e.target.files[0])
 }
 
 function preventDefaults(e) {
-    e.preventDefault()
+  e.preventDefault()
+}
+
+function isImage(file) {
+  return file && file['type'].split('/')[0] === 'image'
 }
 
 const events = ['dragenter', 'dragover', 'dragleave', 'drop']
 
 onMounted(() => {
-    events.forEach((eventName) => {
-        document.body.addEventListener(eventName, preventDefaults)
-    })
+  events.forEach((eventName) => {
+      document.body.addEventListener(eventName, preventDefaults)
+  })
 })
 
 onUnmounted(() => {
-    events.forEach((eventName) => {
-        document.body.removeEventListener(eventName, preventDefaults)
-    })
+  events.forEach((eventName) => {
+      document.body.removeEventListener(eventName, preventDefaults)
+  })
 })
 </script>
